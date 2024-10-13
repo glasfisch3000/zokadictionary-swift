5bec92c5f288b5008a99819a781300dbe6aa81c2
glasfisch3000 <jakob@danckwerts.net>
2024-10-13 12:13:23 +0200

    add migrate command

---
+++ Sources/App/Commands/Migrate.swift
@@ -0,0 +1,58 @@
+import ArgumentParser
+import Vapor
+import NIOFileSystem
+
+struct Migrate: AsyncParsableCommand {
+    static let configuration = CommandConfiguration(
+        commandName: "migrate",
+//        abstract: <#T##String#>,
+//        usage: <#T##String?#>,
+//        discussion: <#T##String#>,
+        version: "0.0.0",
+        shouldDisplay: true,
+        subcommands: [],
+        groupedSubcommands: [],
+        defaultSubcommand: nil,
+        helpNames: .shortAndLong,
+        aliases: []
+    )
+    
+    @ArgumentParser.Option(name: [.short, .customLong("env")])
+    private var environment: ParsableEnvironment = .development
+    
+    @ArgumentParser.Option(name: .shortAndLong)
+    private var configFile: FilePath?
+    
+    @ArgumentParser.Flag(name: .long)
+    private var revert: Bool = false
+    
+    init() { }
+    
+    func run() async throws {
+        let config = try await readAppConfig(path: configFile)
+        
+        var env = environment.makeEnvironment()
+        env.commandInput.arguments = ["migrate"] + (revert ? ["--revert"] : [])
+        
+        try LoggingSystem.bootstrap(from: &env)
+        let app = try await Application.make(env)
+
+        // This attempts to install NIO as the Swift Concurrency global executor.
+        // You can enable it if you'd like to reduce the amount of context switching between NIO and Swift Concurrency.
+        // Note: this has caused issues with some libraries that use `.wait()` and cleanly shutting down.
+        // If enabled, you should be careful about calling async functions before this point as it can cause assertion failures.
+        // let executorTakeoverSuccess = NIOSingletons.unsafeTryInstallSingletonPosixEventLoopGroupAsConcurrencyGlobalExecutor()
+        // app.logger.debug("Tried to install SwiftNIO's EventLoopGroup as Swift's global concurrency executor", metadata: ["success": .stringConvertible(executorTakeoverSuccess)])
+        
+        do {
+            try await configureDB(app, config)
+        } catch {
+            app.logger.report(error: error)
+            try? await app.asyncShutdown()
+            throw error
+        }
+        
+        try await app.execute()
+        try await app.asyncShutdown()
+    }
+}
