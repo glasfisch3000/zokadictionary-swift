8e2714ad0609b0155fbf07275d57da08d464aa1f
glasfisch3000 <jakob@danckwerts.net>
2024-10-14 02:41:26 +0200

    add routes command

---
+++ Sources/App/Commands/Routes.swift
@@ -0,0 +1,56 @@
+import ArgumentParser
+import Vapor
+import NIOFileSystem
+
+struct Routes: AsyncParsableCommand {
+    static let configuration = CommandConfiguration(
+        commandName: "routes",
+        abstract: "Show http server routes.",
+//        usage: <#T##String?#>,
+//        discussion: <#T##String#>,
+        version: "0.0.0",
+        shouldDisplay: true,
+        subcommands: [],
+        groupedSubcommands: [],
+        defaultSubcommand: nil,
+        helpNames: .shortAndLong,
+        aliases: []
+    )
+    
+    @ArgumentParser.Option(name: [.short, .customLong("env")])
+    private var environment: ParsableEnvironment?
+    
+    @ArgumentParser.Option(name: .shortAndLong)
+    private var configFile: FilePath?
+    
+    init() { }
+    
+    func run() async throws {
+        let config = try await readAppConfig(path: configFile)
+        
+        let environment = self.environment ?? config.environment
+        var env = environment.makeEnvironment()
+        env.commandInput.arguments = ["routes"]
+        
+        try LoggingSystem.bootstrap(from: &env)
+        let app = try await Application.make(env)
+
+        // This attempts to install NIO as the Swift Concurrency global executor.
+        // You can enable it if you'd like to reduce the amount of context switching between NIO and Swift Concurrency.
+        // Note: this has caused issues with some libraries that use `.wait()` and cleanly shutting down.
+        // If enabled, you should be careful about calling async functions before this point as it can cause assertion failures.
+        // let executorTakeoverSuccess = NIOSingletons.unsafeTryInstallSingletonPosixEventLoopGroupAsConcurrencyGlobalExecutor()
+        // app.logger.debug("Tried to install SwiftNIO's EventLoopGroup as Swift's global concurrency executor", metadata: ["success": .stringConvertible(executorTakeoverSuccess)])
+        
+        do {
+            try configureRoutes(app)
+        } catch {
+            app.logger.report(error: error)
+            try? await app.asyncShutdown()
+            throw error
+        }
+        
+        try await app.execute()
+        try await app.asyncShutdown()
+    }
+}
